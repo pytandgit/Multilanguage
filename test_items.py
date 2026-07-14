2e38e8de4d1a0fbf8e2b9d5db17a082cb671462c
Alex <stikhinsky@gmail.com>
2026-07-14 14:17:16 +0300

    переименование тестового метода на test_browser_localization

--- test_items.py
+++ test_items.py
@@ -5,8 +5,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def test_multilanguage(browser):
+def test_browser_localization(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    time.sleep(30)
     login_button = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn-add-to-basket")))
-    time.sleep(5)
     assert login_button, "Кнопка 'Добавить в корзину' отсутствует"
